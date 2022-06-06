e7304c14f46a3da5b6851145b2436c71d020e859
maximpantschenko <50883771+maximpantschenko@users.noreply.github.com>
2022-06-06 21:05:13 +0200

    implemented json store

--- src/models/db.js
+++ src/models/db.js
@@ -2,14 +2,18 @@ import { userMemStore } from "./mem/user-mem-store.js";
 import { publistMemStore } from "./mem/publist-mem-store.js";
 import { pubMemStore } from "./mem/pub-mem-store.js";
 
+import { userJsonStore } from "./json/user-json-store.js";
+import { publistJsonStore } from "./json/publist-json-store.js";
+import { pubJsonStore } from "./json/pub-json-store.js";
+
 export const db = {
   userStore: null,
   publistStore: null,
   pubStore: null,
 
   init() {
-    this.userStore = userMemStore;
-    this.publistStore = publistMemStore;
-    this.pubStore = pubMemStore;
+    this.userStore = userJsonStore;
+    this.publistStore = publistJsonStore;
+    this.pubStore = pubJsonStore;
   },
 };
---
+++ src/models/json/pub-json-store.js
@@ -0,0 +1,51 @@
+import { v4 } from "uuid";
+
+import { JSONFile, Low } from "lowdb";
+
+const db = new Low(new JSONFile("./src/models/json/pubs.json"));
+db.data = { pubs: [] };
+
+export const pubJsonStore = {
+    async getAllPubs(){
+        await db.read();
+        return db.data.pubs;
+    },
+
+    async addPub(publistId, pub){
+        await db.read();
+        pub._id = v4();
+        pub.publistid = publistId;
+        db.data.pubs.push(pub);
+        await db.write();
+        return pub;
+    },
+
+    async getPubsByPublistId(id){
+        await db.read();
+        return db.data.pubs.filter((pub) => pub.publistid === id);
+    },
+
+    async getPubById(id){
+        await db.read();
+        return db.data.pubs.find((pub) => pub._id === id);
+    },
+
+    async deletePub(id){
+        await db.read();
+        const index = db.data.pubs.findIndex((pub) => pub._id === id);
+        db.data.pubs.splice(index, 1);
+        await db.write();
+    },
+
+    async deleteAllPubs(){
+        db.data.pubs = [];
+        await db.write();
+    },
+
+    async updatePub(pub, updatePub){
+        pub.name = updatePub.name,
+        pub.city = updatePub.city,
+        pub.country = updatePub.country,
+        await db.write();
+    },
+};
